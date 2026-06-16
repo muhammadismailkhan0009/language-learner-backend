f102a58e4353c05791cce08a2cbc0eb1743b1eb2
muhammadismailkhan0009 <muhammadismailkhan0009@gmail.com>
2026-06-16 08:42:30 +0200

    - f

--- src/main/resources/db/migration/V18__user_profiles_separate_profile_id.sql
+++ src/main/resources/db/migration/V18__user_profiles_separate_profile_id.sql
@@ -1,9 +1,13 @@
+drop table if exists user_profiles;
+
 create table user_profiles (
+    id varchar(255) not null,
     user_id varchar(255) not null,
     difficulty_level varchar(255) not null default 'A1',
     created_at timestamp with time zone not null,
     updated_at timestamp with time zone not null,
-    primary key (user_id),
+    primary key (id),
+    constraint uk_user_profiles_user_id unique (user_id),
     constraint fk_user_profiles_user_info
         foreign key (user_id)
             references user_info (id)
